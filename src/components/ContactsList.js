4fda97b5691c0dea20bc61bb590acc02424163e4
arbaazmeghani <arbaazmeghani@gmail.com>
2019-06-25 20:38:13 -0500

    Add our axios get request to the contacts-rest-api and log the results

--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -2,6 +2,12 @@ import React from 'react';
 import axios from 'axios';
 
 export default class ContactsList extends React.Component {
+    componentDidMount() {
+        axios.get("https://contacts-rest-api.herokuapp.com/api/v1/contacts")
+        .then(res => {
+            console.log(res);
+        });
+    }
 
     render() {
         return (
