9556173ab6b67750135685f71bf18819bc206d18
arbaazmeghani <arbaazmeghani@gmail.com>
2019-06-26 19:25:44 -0500

    Add onclick handler to handle a single contact.  Also add a unique key for each contact using the id property of each contact.

--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -2,13 +2,18 @@ import React from 'react';
 import axios from 'axios';
 
 export default class ContactsList extends React.Component {
-    state = {
-        contacts: []
-    };
+    constructor(props) {
+        super(props);
+        this.state = {contacts: []};
+    }
 
     async componentDidMount() {
         let res = await axios.get("https://contacts-rest-api.herokuapp.com/api/v1/contacts")
         this.setState({contacts: res.data});
+    }
+
+    singleContactHandler(contact) {
+        console.log(contact.firstname);
     }
 
     render() {
@@ -16,7 +21,7 @@ export default class ContactsList extends React.Component {
             <div>
                 {
                     this.state.contacts.map(contact => (
-                        <div>
+                        <div onClick={() => this.singleContactHandler(contact)} key={contact._id}>
                             <h2> {contact.firstname} {contact.lastname} </h2>
                             <h3> {contact.number} </h3>
                             <br />
