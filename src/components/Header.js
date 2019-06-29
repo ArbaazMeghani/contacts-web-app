d45b488e327f542f3af7af97a6e559e1bb80355d
arbaazmeghani <arbaazmeghani@gmail.com>
2019-06-28 21:31:34 -0500

    Add a divider to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 
 function Header() {
     return (
         <header>
             <Typography align="center" variant="h6"> Contacts List Application </Typography>
+            <Divider />
             <br />
         </header>
     );
