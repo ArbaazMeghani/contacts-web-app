80843bf8fb8d99d4f8cc739a17368798ccdcdebe
arbaazmeghani <arbaazmeghani@gmail.com>
2019-06-30 15:07:20 -0500

    Modify FAB to appear floating in botto-right corner

--- src/components/NewContact.js
+++ src/components/NewContact.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/styles';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 
-export default class NewContact extends React.Component {
-    render() {
-        return (
-            <div>
-                 <Fab color="primary" aria-label="Add">
-                    <AddIcon />
-                </Fab>
-            </div>
-        );
-    }
+const styles = {
+  fab: {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    left: 'auto',
+    position: 'fixed',
+  }
+};
+
+class NewContact extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Fab color="primary" aria-label="Add" className={classes.fab}>
+          <AddIcon />
+        </Fab>
+      </div>
+    );
+  }
 }
+
+NewContact.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+  
+
+export default withStyles(styles)(NewContact);
