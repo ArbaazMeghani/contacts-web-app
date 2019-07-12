39b98e696660961723497ae53c55b31c8c0a40ed
arbaazmeghani <arbaazmeghani@gmail.com>
2019-07-11 19:51:15 -0500

    Update ViewSingleContact component class name

--- src/components/ViewSingleContact.js
+++ src/components/ViewSingleContact.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import SingleContact from './SingleContact';
 
-export default class NewContactForm extends React.Component {
+export default class ViewSingleContact extends React.Component {
   render() {
     return (
       <Dialog open={this.props.open} aria-labelledby="simple-dialog-title">
         <form>
-          <SingleContact contact={{}}/>
+          <SingleContact contact={this.props.contact}/>
         </form>
       </Dialog>
     );
