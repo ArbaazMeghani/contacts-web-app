c256a32b5de84045bf1ebfdcaf9bb0c0a11e675c
arbaazmeghani <arbaazmeghani@gmail.com>
2019-07-12 13:12:13 -0500

    Add isDisabled state so we can toggle whether the textfields are disabled or not

--- src/components/ViewSingleContact.js
+++ src/components/ViewSingleContact.js
@@ -3,11 +3,24 @@ import Dialog from '@material-ui/core/Dialog';
 import SingleContact from './SingleContact';
 
 export default class ViewSingleContact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDisabled: true
+    };
+  }
+
+  disabledStateUpdate() {
+    this.setState(prevState => ({
+      isDisabled: !prevState.isDisabled
+    }));
+  }
+
   render() {
     return (
       <Dialog open={this.props.open} aria-labelledby="simple-dialog-title">
         <form>
-          <SingleContact contact={this.props.contact} isDisabled={true}/>
+          <SingleContact contact={this.props.contact} isDisabled={this.state.isDisabled} />
         </form>
       </Dialog>
     );
